91d44cf11bea738e89fb4493647d8dfeebf8408a
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-04 14:41:57 +0200

    booleans

--- #2.Lopps.py
+++ #2.Lopps.py
@@ -1,12 +1,15 @@
-blockchain = [[19]]
-
+blockchain = []
 
 def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value(transaction_amount, last_value):
+    if (last_value == None):
+        last_value = 1
+    blockchain.append([last_value, transaction_amount])
 
 
 def get_transaction_value():
@@ -34,7 +37,8 @@ while (True):
     user_choice = get_user_choice()
     if user_choice == "1":
         user_value = get_transaction_value()
-        add_value(user_value)
+        add_value(transaction_amount=user_value,
+                  last_value=get_last_blockchain_value())
     elif user_choice == "2":
         print_blockchain()
     elif user_choice == "q":
---
+++ #3.booleans.py
@@ -0,0 +1,8 @@
+
+list_1 = [1,2,3,4]
+list_2 = [1,2,3,4]
+
+print("list_1 == list_2 ", list_1 == list_2)
+print("list_1 is list_2 ", list_1 is list_2)
+
+# We can use the "is" operator to check if two variables point to the same object in memory
