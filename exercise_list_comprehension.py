e772f5972b1a8b439f69636cb0416fe3548e905c
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-05 18:58:26 +0200

    andrei

---
+++ exercise_list_comprehension.py
@@ -0,0 +1,9 @@
+simple_list = [1, 2, 3, 4, 5]
+doubled_list = []
+for el in simple_list:
+    doubled_list.append(el * 2)
+
+print(doubled_list)
+
+comprehension_list = [el*2 for el in simple_list]
+print(comprehension_list)
